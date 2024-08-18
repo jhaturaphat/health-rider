42124e2754a1c8ce3e076e7a566685d54b744c52
jhaturaphat <umt.eng52@gmail.com>
2024-08-18 09:04:56 +0700

    from seccess

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,12 +18,13 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div id="app" style="background-color: #E5F6F5">
+        <nav class="navbar navbar-expand-md navbar-light bg-success shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -42,13 +43,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
--- resources/views/orderrider.blade.php
+++ resources/views/orderrider.blade.php
@@ -2,17 +2,31 @@
 
 @section('content')
 <div class="container mt-5">
-    <h2 class="text-success">ลงทะเบียนขอรับยา</h2>
+    <h2 class="text-success ">ลงทะเบียนขอรับยา</h2>
+    <hr>
     <form>
         <div class="row">
             <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <sapn for="birth_date"  class="input-group-text text-white" for="hn">HN</sapn>
+                            <input type="text" class="form-control" id="hn" value="000088973">
+                            <span class="input-group-text text-white" title="ค้นหา"><i class="bi bi-search"></i></span>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="coverageDesc" class="input-group-text text-white">สิทธิการรักษา</label>
+                            <input type="text" class="form-control" id="coverageDesc" value="ประกันสังคม รพ.ทดสอบ">
+                        </div>
+                    </div>
+                </div>
                 <!-- Row for Name Fields -->
                 <div class="row">
                     <div class="col-md-3">
-                        <div class="mb-3 input-group input-group-sm">
-                            {{-- <label for="account_title_th" class="form-label">คำนำหน้าชื่อ</label>
-                            <input type="text" class="form-control" id="account_title_th" value="นาย"> --}}
-                            <span class="input-group-text text-white" id="account_title_th">คำนำหน้าชื่อ</span>
+                        <div class="mb-3 input-group input-group-sm">                            
+                            <span class="input-group-text text-white" for="account_title_th">คำนำหน้าชื่อ</span>
                             <input type="text" class="form-control" id="account_title_th" value="นาย" aria-describedby="basic-addon3">
                         </div>
                     </div>
@@ -30,87 +44,138 @@
                     </div>
                 </div>
                  {{-- --}} 
-                <div class="mb-3">
-                    <label for="birth_date" class="form-label">วันเกิด</label>
-                    <input type="date" class="form-control" id="birth_date" value="1999-04-22">
-                </div>
-                <div class="mb-3">
-                    <label for="gender" class="form-label">เพศ</label>
-                    <input type="text" class="form-control" id="gender" value="ชาย">
-                </div>
-                <div class="mb-3">
-                    <label for="mobile" class="form-label">เบอร์โทรศัพท์</label>
-                    <input type="text" class="form-control" id="mobile" value="0987654321">
-                </div>
-                <div class="mb-3">
-                    <label for="mobile_other" class="form-label">เบอร์โทรศัพท์อื่น</label>
-                    <input type="text" class="form-control" id="mobile_other" value="0995656548">
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">อีเมล</label>
-                    <input type="email" class="form-control" id="email" value="test@gmail.com">
-                </div>
-                <div class="mb-3">
-                    <label for="address" class="form-label">ที่อยู่</label>
+                 <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="birth_date" class="input-group-text text-white">วันเกิด</label>
+                            <input type="date" class="form-control" id="birth_date" value="1999-04-22">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="gender" class="input-group-text text-white" >เพศ</label>
+                            <input type="text" class="form-control" id="gender" value="ชาย">
+                        </div>
+                    </div>                    
+                 </div>
+                
+                 <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="mobile" class="input-group-text text-white">เบอร์โทรศัพท์</label>
+                            <input type="text" class="form-control" id="mobile" value="0987654321">
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="mobile_other" class="input-group-text text-white">เบอร์โทรศัพท์อื่น</label>
+                            <input type="text" class="form-control" id="mobile_other" value="0995656548">
+                        </div>
+                    </div>
+                 </div>
+                
+                <br>
+                <h2 class="text-success">ข้อมูลที่อยู่</h2>
+                <hr>
+                <div class="mb-3 input-group input-group-sm">
+                    <label for="address" class="input-group-text text-white">ที่อยู่</label>
                     <input type="text" class="form-control" id="address" value="125 หมู่ 7 ต.ศรีภูมิ อ.เมือง จ.เชียงใหม่ 50200">
                 </div>
-                <div class="mb-3">
-                    <label for="house_no" class="form-label">บ้านเลขที่</label>
-                    <input type="text" class="form-control" id="house_no" value="125">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="house_no" class="input-group-text text-white">บ้านเลขที่</label>
+                            <input type="text" class="form-control" id="house_no" value="125">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="moo" class="input-group-text text-white">หมู่ที่</label>
+                            <input type="text" class="form-control" id="moo" value="7">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="road" class="input-group-text text-white">ถนน</label>
+                            <input type="text" class="form-control" id="road" value="ถ.คลองน้ำไหล">
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="moo" class="form-label">หมู่ที่</label>
-                    <input type="text" class="form-control" id="moo" value="7">
+                
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="tambon" class="input-group-text text-white">ตำบล</label>
+                            <input type="text" class="form-control" id="tambon" value="ศรีภูมิ">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="amphur" class="input-group-text text-white">อำเภอ</label>
+                            <input type="text" class="form-control" id="amphur" value="เมือง">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="province" class="input-group-text text-white">จังหวัด</label>
+                            <input type="text" class="form-control" id="province" value="เชียงใหม่">
+                        </div>
+                    </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="postal_code" class="input-group-text text-white">รหัสไปรษณีย์</label>
+                            <input type="text" class="form-control" id="postal_code" value="50200">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="email" class="input-group-text text-white">อีเมล</label>
+                            <input type="email" class="form-control" id="email" value="test@gmail.com">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="lat" class="input-group-text text-white">ละติจูด</label>
+                            <input type="text" class="form-control" id="lat" value="18.790519">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="lng" class="input-group-text text-white">ลองจิจูด</label>
+                            <input type="text" class="form-control" id="lng" value="98.970773">
+                        </div>
+                    </div>
+                </div>
+                
             </div>
             <div class="col-md-6">
-                <div class="mb-3">
-                    <label for="road" class="form-label">ถนน</label>
-                    <input type="text" class="form-control" id="road" value="ถ.คลองน้ำไหล">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="drugLocationDesc" class="input-group-text text-white">ตำแหน่งห้องยา</label>
+                            <input type="text" class="form-control" id="drugLocationDesc" value="ห้องจ่ายยา ชั้น 2">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3 input-group input-group-sm">
+                            <label for="cinicLocationDesc" class="input-group-text text-white">ตำแหน่งคลินิก</label>
+                            <input type="text" class="form-control" id="cinicLocationDesc" value="อายุรกรรมต่อมไร้ท่อ">
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="province" class="form-label">จังหวัด</label>
-                    <input type="text" class="form-control" id="province" value="เชียงใหม่">
-                </div>
-                <div class="mb-3">
-                    <label for="tambon" class="form-label">ตำบล</label>
-                    <input type="text" class="form-control" id="tambon" value="ศรีภูมิ">
-                </div>
-                <div class="mb-3">
-                    <label for="amphur" class="form-label">อำเภอ</label>
-                    <input type="text" class="form-control" id="amphur" value="เมือง">
-                </div>
-                <div class="mb-3">
-                    <label for="postal_code" class="form-label">รหัสไปรษณีย์</label>
-                    <input type="text" class="form-control" id="postal_code" value="50200">
-                </div>
-                <div class="mb-3">
-                    <label for="note" class="form-label">หมายเหตุ</label>
-                    <textarea class="form-control" id="note">ทดลองลงทะเบียน</textarea>
-                </div>
-                <div class="mb-3">
-                    <label for="lat" class="form-label">ละติจูด</label>
-                    <input type="text" class="form-control" id="lat" value="18.790519">
-                </div>
-                <div class="mb-3">
-                    <label for="lng" class="form-label">ลองจิจูด</label>
-                    <input type="text" class="form-control" id="lng" value="98.970773">
-                </div>
-                <div class="mb-3">
-                    <label for="drugLocationDesc" class="form-label">ตำแหน่งห้องยา</label>
-                    <input type="text" class="form-control" id="drugLocationDesc" value="ห้องจ่ายยา ชั้น 2">
-                </div>
-                <div class="mb-3">
-                    <label for="cinicLocationDesc" class="form-label">ตำแหน่งคลินิก</label>
-                    <input type="text" class="form-control" id="cinicLocationDesc" value="อายุรกรรมต่อมไร้ท่อ">
-                </div>
-                <div class="mb-3">
-                    <label for="coverageDesc" class="form-label">สิทธิการรักษา</label>
-                    <input type="text" class="form-control" id="coverageDesc" value="ประกันสังคม รพ.ทดสอบ">
-                </div>
-                <div class="mb-3">
-                    <label for="callback_url" class="form-label">Callback URL</label>
+                
+                <div class="mb-3 input-group input-group-sm">
+                    <label for="callback_url" class="input-group-text text-white">Callback URL</label>
                     <input type="text" class="form-control" id="callback_url" value="https://Example_hospital.com/callback">
+                </div>
+                <div class="mb-3 input-group input-group-sm">
+                    <label for="note" class="input-group-text text-white">หมายเหตุ</label>
+                    <textarea class="form-control" id="note">ทดลองลงทะเบียน</textarea>
                 </div>
             </div>
         </div>
